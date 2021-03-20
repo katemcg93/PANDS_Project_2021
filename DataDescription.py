e515a589be0163df1f7524b2a3ac1ec0ff6ba6f8
katemcg93 <katemcg93@gmail.com>
2021-03-20 17:05:30 +0000

    More data description with Pandas

--- DataDescription.py
+++ DataDescription.py
@@ -1,13 +1,23 @@
 #author Kate McGrath
-#This file is an initial attempt at analysing and understanding data, probably won't make it into actual project
-
-#import csv
-#with open ("irisDataset.csv") as csv_file:
-    #data_reader = csv.reader(csv_file, delimiter = ",")
-    #for row in data_reader:
-        #print (row)
+#This file is an initial attempt at analysing and understanding data,  won't make it into actual project
+#Committing it to show my early work on the project
 
 import pandas as pd 
-irisDataSet = pd.read_csv ("IrisDataSet.csv", index_col = "Class")
-print("Sanity")
-print(irisDataSet)
+
+#Getting a feel for what dataset looks like, want to view all data while still getting to grips with it
+#Returning data correct to two decimal places
+pd.set_option("display.precision", 2)
+
+irisDataSet = pd.read_csv ("IrisDataSet.csv")
+print(irisDataSet.head())
+print(irisDataSet.tail())
+
+#Viewing descriptive statistics, although this doesn't tell us much without class data
+print(irisDataSet.describe())
+
+print(irisDataSet.shape)
+print(irisDataSet.columns)
+
+print(irisDataSet["Class"].value_counts())
+
+print(irisDataSet.corr(method = "pearson"))
